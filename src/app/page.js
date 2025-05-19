f925d139104a42ea8451b1df62d5b050633540d4
Maiwan Amini <maiwan.amini@gmail.com>
2025-05-19 15:26:44 +0200

    feat: project structuur met components- en lib-map

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-500">
-      <h1 className="text-white text-4xl font-bold">
-        Tailwind werkt! 🚀
-      </h1>
+      <h1 className="text-white text-3xl font-bold">Tailwind werkt!</h1>
     </div>
   );
 }
-
