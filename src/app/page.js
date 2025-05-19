37150f5d44ef6010a567c48a05aba70561138ed2
Maiwan Amini <maiwan.amini@gmail.com>
2025-05-19 15:17:38 +0200

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-white text-4xl font-bold">
+        Tailwind werkt! 🚀
+      </h1>
+    </div>
+  );
+}
+
